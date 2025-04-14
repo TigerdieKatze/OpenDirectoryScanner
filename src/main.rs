373fdfe6620402ccc2bac008ba1375ec5250361f
Harry Röben <hre@orgadata.ag>
2025-04-14 10:45:38 +0200

    thx rust

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ struct NSFWDetector {
 impl NSFWDetector {
     fn new() -> Result<Self, Box<dyn Error>> {
         // load file from project directory
-        let model_data = include_bytes!("resources/model.onnx");
+        let model_data = include_bytes!("../resources/model.onnx");
         let model = create_model(model_data)?;
 
         Ok(NSFWDetector { model })
